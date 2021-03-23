1637dc7635b12f6faebc8389abb0e477905fbba7
Jireh Lau <jirehcylau@gmail.com>
2021-03-23 00:44:47 -0400

    exercise 2 push

--- exercise-2.py
+++ exercise-2.py
@@ -7,3 +7,6 @@
 #      - What you entered is xx characters long
 # 3. Return to step 1, unless the word 'quit' was entered.
 
+word = input("Enter a word or a phrase")
+while word != "quit":
+    print(f"What you entered is {word} long")
