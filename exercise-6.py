8fbabcda0b12844ee50f698db2c40ca31440f778
Jireh Lau <jirehcylau@gmail.com>
2021-03-23 01:36:51 -0400

    push exercise 6

--- exercise-6.py
+++ exercise-6.py
@@ -6,10 +6,10 @@
 # 2. Then prompts the user to enter the day of the month: 
 #      Enter the day of the month:
 # 3. Calculate what season it is based upon this chart:
-#      Dec 21 - Mar 19: Winter
-#      Mar 20 - Jun 20: Spring
-#      Jun 21 - Sep 21: Summer
-#      Sep 22 - Dec 20: Fall
+#      Dec 21 - Mar 19: Winter jan and feb
+#      Mar 20 - Jun 20: Spring april and may
+#      Jun 21 - Sep 21: Summer july and august
+#      Sep 22 - Dec 20: Fall october and november
 # 4. Print the result as follows:
 #      <Mmm> <dd> is in <season> 
 
@@ -19,3 +19,49 @@
 #
 # After setting the likely season, you can use another if...elif...else statement to "adjust" if
 # the day number falls within a certain range.
+
+months = input("Enter the month of the year (Jan - Dec)")
+day_of_month = input("Enter the day of the month: ")
+
+if month == "Jan" or month == "Feb":
+    print("{month} {day_of_month} is Winter")
+
+elif month == "Apr" or month == "May":
+    print("{month} {day_of_month} is Spring") 
+
+elif month == "Jul" or month == "Aug":
+    print("{month} {day_of_month} is Summer") 
+
+elif month == "Oct" or month == "Nov":
+    print("{month} {day_of_month} is Fall") 
+
+# SPECIFIED WINTER DATES 
+elif month == "Dec" and day >= 21:
+    print("{month} {day_of_month} is Winter")
+
+elif month == "Mar" and day < 20:
+    print("{month} {day_of_month} is Winter")
+
+# SPECIFIED SPRING DATES 
+elif month == "Mar" and day >= 20:
+    print("{month} {day_of_month} is Spring")
+
+elif month == "Jun" and day < 21:
+    print("{month} {day_of_month} is Spring")
+
+# SPECIFIED SUMMER DATES 
+elif month == "Mar" and day >= 21:
+    print("{month} {day_of_month} is Summer")
+
+elif month == "Jun" and day < 22:
+    print("{month} {day_of_month} is Summer")
+
+# SPECIFIC FALL DATES 
+elif month == "Sep" and day >= 22:  
+    print("{month} {day_of_month} is Fall")
+
+elif month == "Dec" and day < 21:
+    print("{month} {day_of_month} is Fall")
+
+else: 
+    print("Invalid")
